72a3569e062302f6eeb77b2ac6904a20828493b7
Aaron Light <aaronl@tx.rr.com>
2023-03-08 22:18:39 -0700

    changed cors to match frontend url but removed slash at end of url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
     #  origins "*"
     # origins 'http://localhost:3000'
-    origins 'https://deploy-branch.d3dbk0hi3hye47.amplifyapp.com/'
+    origins 'https://deploy-branch.d3dbk0hi3hye47.amplifyapp.com'
 
      resource "*",
        headers: :any,
